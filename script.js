b8fbea1a858428899f14e091f17e9e21994dd0f7
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2023-03-27 01:11:37 +0530

    Fix the bug which was not changing grid's color

--- script.js
+++ script.js
@@ -1,75 +1,58 @@
 const main = document.querySelector('.main');
 
+function createHeading() {
+  const headingDiv = document.createElement('div');
+  headingDiv.setAttribute('class', 'heading');
 
-function createHeading(){
-    
-      const headingDiv = document.createElement('div');
-      headingDiv.setAttribute('class', 'heading');
+  const headingPara = document.createElement('p');
+  headingPara.textContent = 'ETCH-A-SKETCH';
 
-      const headingPara = document.createElement('p');
-      headingPara.textContent = 'ETCH-A-SKETCH';
-
-      headingDiv.appendChild(headingPara);
-      main.appendChild(headingDiv);
-    
+  headingDiv.appendChild(headingPara);
+  main.appendChild(headingDiv);
 }
-function generateGrid(gdSize){
+function generateGrid(gdSize) {
+  let gridSize = gdSize;
+  const gridDiv = document.createElement('div');
+  gridDiv.setAttribute('class', 'grid');
 
-    let gridSize = gdSize;
-    const gridDiv = document.createElement('div');
-    gridDiv.setAttribute('class','grid');
+  for (let i = 0; i < gridSize ** 2; i++) {
+    const elSize = 960 / gridSize;
+    const gridElBtn = document.createElement('button');
+    gridElBtn.setAttribute('class', `gridEl${i}`);
 
-    for(let i=0;i<gridSize **2;i++){
+    gridElBtn.style.backgroundColor = 'aquaBlue';
+    gridElBtn.style.width = `${elSize}px`;
+    gridElBtn.style.height = `${elSize}px`;
+    gridDiv.appendChild(gridElBtn);
+  }
 
-        const elSize = 960/gridSize;
-        const gridElBtn = document.createElement('button');
-        gridElBtn.setAttribute('class',`gridEl${i}`);
+  main.appendChild(gridDiv);
+}
+function startHoverOnClick(x) {
+  const gridBtns = document.querySelectorAll('button');
+  gridBtns.forEach((btn) => {
+    btn.addEventListener('click', createHover);
+  });
+}
 
-        gridElBtn.style.backgroundColor = "aquaBlue";
-        gridElBtn.style.width = `${elSize}px`;
-        gridElBtn.style.height = `${elSize}px`;
-        gridDiv.appendChild(gridElBtn);
+function mE(e) {
+  e.target.style.backgroundColor = 'black';
+}
 
+function createHover(e) {
+  const gridBtns = document.querySelectorAll('button');
+  e.target.style.backgroundColor = 'black';
+  gridBtns.forEach((btn) => {
+    if (x % 2 == 0) {
+      btn.addEventListener('mouseenter', mE);
+    } else {
+      btn.removeEventListener('mouseenter', mE);
     }
-    
-    main.appendChild(gridDiv);
-
-}
-function startHoverOnClick(x){
-
-    const gridBtns = document.querySelectorAll('button');
-    gridBtns.forEach((btn) => {
-        
-        if(x%2==0){
-            btn.addEventListener('click', createHover);
-        } else{
-            btn.removeEventListener('click',createHover);
-        }
-    });
-    
+  });
+  x++;
 }
 
-function mE(e){
-     e.target.style.backgroundColor = 'black';
-}
-
-function createHover(e){
-
-    
-    const gridBtns = document.querySelectorAll('button');
-    e.target.style.backgroundColor = "black";
-    gridBtns.forEach((btn)=>{
-        if(x%2==0){
-               btn.addEventListener('mouseenter', mE);
-               
-        } else{
-               btn.removeEventListener('mouseenter',mE);
-        }
-    });
-    x++;
-}
-
-let x =0;
+let x = 0;
 createHeading();
 generateGrid(16);
 startHoverOnClick(x);
